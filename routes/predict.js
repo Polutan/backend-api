e5aefbfd44673b1c60dd66132697c7b609999445
nabillapw <nabillaputri230501@gmail.com>
2022-06-11 21:14:12 +0700

    initial commit

--- routes/predict.js
+++ routes/predict.js
@@ -6,7 +6,7 @@ router.get('/', (req, res) => {
     res.status(200).json({
         error: false,
         status: res.statusCode,
-        message: 'Predict Success!',
+        message: 'Prediction Success!',
         score
     })
 })
